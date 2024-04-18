d1251df9a15c568c2108fac95211e0ab6176c59e
Parvin Agayev <a.parvin@ultra.az>
2024-04-18 14:47:51 +0400

    some changes

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ let datas = [];
 
 // ------------------------ select tbody's item to delete --------------
 
-document.querySelectorAll('.tr-body').forEach(function (tr) {
-    tr.addEventListener('click', function () {
-        this.style.backgroundColor = 'red';
-        datas.push(this);
+function addEventListenerToAllTr(){
+    document.querySelectorAll('.tr-body').forEach(function (tr) {
+        tr.addEventListener('click', function () {
+            document.querySelector('.trash-btn').removeAttribute('disabled');
+            this.style.backgroundColor = 'red';
+            datas.push(this);
+        });
     });
-});
+    
+};
 
 
+addEventListenerToAllTr();
 
 // ------------ remove tbody's items ------------------
 
@@ -26,7 +31,28 @@ document.querySelector('.trash-btn').addEventListener('click', function () {
             }
         }
     }
+    document.querySelector('.trash-btn').setAttribute('disabled', 'disabled');
 });
+
+
+
+// ------------------------- add data tbody -------------------------
+document.querySelector('.add-btn').addEventListener('click', function () {
+    let tbody = document.querySelector('.tbody');
+    let newRow = document.createElement('tr');
+    newRow.classList.add("tr-body");
+    for (let i = 0; i < 4; i++) {
+        let newCell = document.createElement('td');
+        newCell.textContent = "fake";
+        newRow.appendChild(newCell);
+    };
+    tbody.appendChild(newRow);
+    addEventListenerToAllTr();
+});
+
+
+
+
 
 
 
